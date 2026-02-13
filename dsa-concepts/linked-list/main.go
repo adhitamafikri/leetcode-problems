6c6e6de28c131567c94049b20259142b012e9db7
Adhitama Fikri <adhitamafikri.dev@gmail.com>
2026-02-13 15:17:16 +0700

    feat: improve linked list appraoch

--- dsa-concepts/linked-list/main.go
+++ dsa-concepts/linked-list/main.go
@@ -2,88 +2,49 @@ package main
 
 import "fmt"
 
-type LinkedList struct {
-	head  int
-	tail  int
-	nodes *LinkedListNode
+// List represents a singly-linked list that holds
+// values of any type.
+type List[T any] struct {
+	head, tail *element[T]
 }
 
-type LinkedListNode struct {
-	id   int
-	data string
-	next *LinkedListNode
+type element[T any] struct {
+	next *element[T]
+	val  T
 }
 
-// Recursively traverse the '.next' property of each node, until the supplied 'node' is nil.
-// Which means the current iteration has no actual node value
-func (l *LinkedList) traverseAndPrint(node *LinkedListNode) {
-	if node != nil {
-		fmt.Println("List item: ", node)
-		l.traverseAndPrint(node.next)
-	}
-}
+func (l *List[T]) Push(val T) {
+	fmt.Println("Pushing val: ", val)
 
-func (l *LinkedList) traverseAndAppend(node *LinkedListNode, newNode *LinkedListNode) {
-	if node.next != nil {
-		l.traverseAndAppend(node.next, newNode)
+	if l.tail == nil {
+		fmt.Println("l.tail == nil", val)
+		l.head = &element[T]{next: nil, val: val}
+		l.tail = l.head
 	} else {
-		node.next = newNode
+		fmt.Println("l.tail != nil", val)
+		l.tail.next = &element[T]{next: nil, val: val}
+		l.tail = l.tail.next
 	}
 }
 
-func (l *LinkedList) appendNode(data string) {
-	var newNode LinkedListNode
-	if l.head < 0 && l.tail < 0 {
-		l.head = 0
-		l.tail = 0
-		newNode = LinkedListNode{
-			id:   0,
-			data: data,
-			next: nil,
-		}
-		l.nodes = &newNode
-	} else {
-		l.tail++
-		newNode = LinkedListNode{
-			id:   l.tail,
-			data: data,
-			next: nil,
-		}
-		l.traverseAndAppend(l.nodes, &newNode)
+func (l *List[T]) AllValues() []T {
+	var values []T
+
+	for e := l.head; e != nil; e = e.next {
+		values = append(values, e.val)
 	}
+
+	return values
 }
 
 func main() {
-	fmt.Println("This is the linked list again")
-	list := LinkedList{
-		head:  -1,
-		tail:  -1,
-		nodes: nil,
+	sVal := []string{"Adhitama", "Fikri", "K"}
+	var sList List[string]
+
+	for _, val := range sVal {
+		sList.Push(val)
 	}
-	fmt.Println("My Linked List: ", list, "\n")
 
-	fmt.Println("\nTraversing and Appending New Item to Linked List\n---------------")
-	list.appendNode("Item #1")
-	fmt.Println("My Linked List: ", list, "\n")
-
-	list.appendNode("Item #2")
-	fmt.Println("My Linked List: ", list, "\n")
-
-	list.appendNode("Item #3")
-	fmt.Println("My Linked List: ", list, "\n")
-
-	list.appendNode("Item #4")
-	fmt.Println("My Linked List: ", list, "\n")
-
-	list.appendNode("Item #5")
-	fmt.Println("My Linked List: ", list, "\n")
-
-	list.appendNode("Item #6")
-	fmt.Println("My Linked List: ", list, "\n")
-
-	list.appendNode("Item #7")
-	fmt.Println("My Linked List: ", list, "\n")
-
-	fmt.Println("\nTraversing Linked List item\n---------------")
-	list.traverseAndPrint(list.nodes)
+	fmt.Println("This is the list", sList)
+	fmt.Println("All values from the list: ", sList.AllValues())
 }
