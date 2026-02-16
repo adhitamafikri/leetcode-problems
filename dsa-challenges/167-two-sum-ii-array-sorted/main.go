e4d0c82c93c1a35f14fe49f65ac34b50c8ad81ed
Adhitama Fikri <adhitamafikri.dev@gmail.com>
2026-02-16 08:32:02 +0700

    feat(dsa-challenges): 167 two sum II, optimal way

--- dsa-challenges/167-two-sum-ii-array-sorted/main.go
+++ dsa-challenges/167-two-sum-ii-array-sorted/main.go
@@ -1,7 +1,7 @@
-// Naive way
 package main
 
-func twoSum(numbers []int, target int) []int {
+// Naive way
+func twoSumNaive(numbers []int, target int) []int {
 	length := len(numbers)
 
 	for i := 0; i < length; i++ {
@@ -9,6 +9,36 @@ func twoSum(numbers []int, target int) []int {
 			if numbers[i]+numbers[j] == target {
 				return []int{i + 1, j + 1}
 			}
+		}
+	}
+
+	return []int{}
+}
+
+// Optimal way
+func twoSum(numbers []int, target int) []int {
+	length := len(numbers)
+	lp := 0
+	rp := length - 1
+
+	sum := 0
+	for {
+		sum = numbers[lp] + numbers[rp]
+
+		if sum == target {
+			return []int{lp + 1, rp + 1}
+		}
+
+		if sum > target {
+			rp--
+		}
+
+		if sum < target {
+			lp++
+		}
+
+		if lp >= rp {
+			break
 		}
 	}
 
--- dsa-challenges/167-two-sum-ii-array-sorted/main.ts
+++ dsa-challenges/167-two-sum-ii-array-sorted/main.ts
@@ -1,5 +1,5 @@
 // Naive way
-function twoSum(numbers: number[], target: number): number[] {
+function twoSumNaive(numbers: number[], target: number): number[] {
   const len = numbers.length
   let lp = 0
   let rp = lp + 1
@@ -10,6 +10,39 @@ function twoSum(numbers: number[], target: number): number[] {
           if (numbers[i] + numbers[j] === target) {
               return [i + 1, j + 1]
           }
+      }
+  }
+
+  return result
+};
+
+// Optimal way
+function twoSum(numbers: number[], target: number): number[] {
+  const len = numbers.length
+  let lp = 0
+  let rp = len - 1
+  const result: number[] = []
+
+  // sum > target -> move the right ptr to left
+  // sum < target -> move the left ptr to right
+
+  let sum = 0
+  while(true) {
+      sum = numbers[lp] + numbers[rp]
+      if (sum === target) {
+          return [lp+1, rp+1]
+      }
+
+      if (sum > target) {
+          rp--
+      }
+
+      if (sum < target) {
+          lp++
+      }
+
+      if (lp >= rp) {
+          break
       }
   }
 
